318d8bcbdead8791dcd5d8649b12cef1d1870a65
abiancu <alejandrobiancucci@gmail.com>
2018-04-15 12:30:17 -0500

    Sorting works

--- BubbleSort/Program.cs
+++ BubbleSort/Program.cs
@@ -4,9 +4,34 @@ namespace BubbleSort
 {
     class Program
     {
-        static void Main(string[] args)
+   
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            int[] _array = { 800, 11, 50, 771, 649, 770, 240, 9 };
+            int parkingLot;            
+            bool sorted = false;
+            //SORTING THE ARRAY
+            while (!sorted)
+            {
+                sorted = true;
+                //SCANING THE ARRAY
+                for (var i = 0; i < _array.Length -1; i++)
+                {
+                    if (_array[i] > _array[i + 1])
+                    {
+                        parkingLot = _array[i];
+                        _array[i] = _array[i + 1];
+                        _array[i + 1] = parkingLot;
+                        sorted = false;
+                    }
+                }
+            }foreach(var number in _array)
+            {
+                Console.WriteLine("{0}", number);
+            }         
+      
+        Console.ReadLine();
+
         }
     }
 }
