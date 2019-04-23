322ef378bce41c8358fea60ca7e344a28ca09cdf
y.tsokota <y.tsokota@gmail.com>
2019-04-23 15:19:22 +0300

    Its more correct

--- BubbleSort/Program.cs
+++ BubbleSort/Program.cs
@@ -1,40 +1,34 @@
 ﻿using System;
+using System.Threading.Tasks.Dataflow;
 
 namespace BubbleSort
 {
     class Program
     {
-   
         static void Main()
         {
             int[] _array = { 800, 11, 50, 771, 649, 5, 770, 240, 9, 54, 67, 8899, 2 };
-            int parkingLot;            
-            bool sorted = false;
             //SORTING THE ARRAY
-            while (!sorted)
+            for (var j = 0; j < _array.Length; j++)
             {
-                sorted = true;
                 //SCANING THE ARRAY
-                for (var i = 0; i < _array.Length -1; i++)
+                for (var i = 0; i < j; i++)
                 {
                     //COMPARING THE VALUES
-                    if (_array[i] > _array[i + 1])
+                    if (_array[i] > _array[j])
                     {
                         //SWAPPING VALUES TO THE CORRECT PLACE
-                        parkingLot = _array[i];
-                        _array[i] = _array[i + 1];
-                        _array[i + 1] = parkingLot;
-                        sorted = false;
+                        int temp = _array[i];
+                        _array[i] = _array[j];
+                        _array[j] = temp;
                     }
                 }
             }
-            foreach (var number in _array)
-            {
-                Console.WriteLine("{0}", number);
-            }         
-      
-        Console.ReadLine();
 
+            foreach (var number in _array)
+                Console.WriteLine("{0} ", number);
+
+            Console.ReadLine();
         }
     }
 }
